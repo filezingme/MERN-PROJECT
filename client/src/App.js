611b15eae9114fd6c0e7104edee15035400ecf13
taitd <tai.td@citigo.net>
2022-08-23 15:25:46 +0700

    Connect Service/API & handle Login

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,30 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Landing from './components/layout/Landing'
 import Auth from './views/Auth'
+import AuthContextProvider from './contexts/AuthContext'
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <AuthContextProvider>
+      <Router>
+        <Routes>
 
-        <Route path="/" element={<Landing />} /> {/* <Route path="/" element={<Navigate replace to="/login" />} /> */}
-        
-        <Route 
-          path="/login" 
-          element={<Auth authRoute='login'  />}
-          exact />
-        
-        <Route 
-          path="/register" 
-          element={<Auth authRoute='register'  />}
-          exact />
+          <Route path="/" element={<Landing />} /> {/* <Route path="/" element={<Navigate replace to="/login" />} /> */}
+          
+          <Route 
+            path="/login" 
+            element={<Auth authRoute='login'  />}
+            exact />
+          
+          <Route 
+            path="/register" 
+            element={<Auth authRoute='register'  />}
+            exact />
 
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </AuthContextProvider>    
   );
 }
 
--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -2,16 +2,46 @@
 // import Form from 'react-bootstrap/Form'
 import { Button, Form } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import {useState, useContext} from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 const LoginForm = () => {
+
+    //Context
+    const {loginUser} = useContext(AuthContext)
+
+    const [loginForm, setLoginForm] = useState({
+      username: '',
+      password: ''
+    })
+
+    const {username, password} = loginForm
+
+    const onChangeLoginForm = event => setLoginForm({
+      ...loginForm,
+      [event.target.name]: event.target.value
+    })
+
+    const login = async event => {
+      event.preventDefault()
+
+      try {
+        
+        const loginData = await loginUser(loginForm)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
     return (<>
-      <Form className='my-4'>
+      <Form className='my-4' onSubmit={login}>
         <Form.Group>
-          <Form.Control type='text' placeholder='Username' name='username' className='my-3' required />
+          <Form.Control type='text' placeholder='Username' name='username' className='my-3' required value={username} onChange={onChangeLoginForm} />
         </Form.Group>
         
         <Form.Group>
-          <Form.Control type='password' placeholder='Password' name='password' className='my-3' required />
+          <Form.Control type='password' placeholder='Password' name='password' className='my-3' required value={password} onChange={onChangeLoginForm} />
         </Form.Group>
         
         <Button variant='success' type='submit' className='my-3'>Login</Button>
