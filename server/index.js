228ea672791d9d98330e777bdc489452aa7eac24
taitd <tai.td@citigo.net>
2022-09-09 16:22:17 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -39,6 +39,6 @@ app.use(cors())
 app.use('/api/auth', authRoute)
 app.use('/api/posts', postRoute)
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
