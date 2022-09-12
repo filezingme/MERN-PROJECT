e9590549a9495a656f76c67d55ac92ba4c8df04d
taitd <tai.td@citigo.net>
2022-09-12 12:39:35 +0700

    prepare deploy

--- client/src/constants/commonContant.js
+++ client/src/constants/commonContant.js
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'production-api-url'
+export const API_URL = 
+process.env.NODE_ENV !== 'production' 
+? 'http://localhost:5000/api' 
+: 'https://peaceful-island-09812.herokuapp.com/api'
