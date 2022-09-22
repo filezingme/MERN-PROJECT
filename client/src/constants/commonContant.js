82146b41f93351873973b35cd0e6e14a66512265
taitd <tai.td@citigo.net>
2022-09-22 15:02:04 +0700

    commit

--- client/src/constants/commonContant.js
+++ client/src/constants/commonContant.js
@@ -1,4 +1,4 @@
 export const API_URL = 
-process.env.NODE_ENV === 'production' 
+process.env.NODE_ENV !== 'production' 
 ? 'http://localhost:5000/api' 
-: 'https://damp-eyrie-82052.herokuapp.com/api'
+: 'https://serene-brook-53814.herokuapp.com/api'
