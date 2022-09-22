9aeba2b051a0c1d56dcdc2f066bd08fa0b88fadc
taitd <tai.td@citigo.net>
2022-09-22 14:16:41 +0700

    'commit'

--- server/index.js
+++ server/index.js
@@ -27,18 +27,56 @@ const connectDB = async() => {
     }
 }
 
+
 connectDB()
+
 
 const app = express()
 
-app.use(express.json())
+// cors: use it before all route definitions
+//ref: https://www.npmjs.com/package/cors
+
+//1.
 app.use(cors())
 
-//app.get('/', (req, res) => res.send('Hello world'))
+//2.
+//app.use(cors({origin: '*'}))
 
+//3.
+//app.use(cors({origin: ['http://localhost:3000', 'https://localhost:3000','http://glacial-chamber-01131.herokuapp.com','https://glacial-chamber-01131.herokuapp.com']}));
+
+//4.
+// const corsOptions = {
+//     origin:'*', 
+//     credentials:true, //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+
+//5.
+// const whitelist = ["http://localhost:3000"]
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+// app.use(cors(corsOptions))
+
+
+app.use(express.json())
+
+
+//app.get('/', (req, res) => res.send('Hello world'))
 app.use('/api/auth', authRoute)
 app.use('/api/posts', postRoute)
 
+
 const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
